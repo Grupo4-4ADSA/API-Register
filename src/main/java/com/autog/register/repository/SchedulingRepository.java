c7c225b2b52bc79e13e7504276f294825964b97e
jonas.junior <jonas.junior@valemobi.com.br>
2022-09-12 20:49:36 -0300

    salvando progresso

--- src/main/java/com/autog/register/repository/SchedulingRepository.java
+++ src/main/java/com/autog/register/repository/SchedulingRepository.java
@@ -2,6 +2,12 @@ package com.autog.register.repository;
 
 import com.autog.register.entity.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SchedulingRepository extends JpaRepository<Scheduling, Integer> {
+
+    @Query("SELECT s FROM Scheduling s where room.building.idBuilding = ?1")
+    List<Scheduling> getSchedulingByCompany(Integer idCompany);
 }
